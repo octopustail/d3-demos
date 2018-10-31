0ea3fc9b97f2b452003b6e173325ea96e27ede12
octopustail <494479914@qq.com>
2018-10-31 15:49:37 +0800

    增加了柱状图的交互效果

--- barChartAnimate/index.js
+++ barChartAnimate/index.js
@@ -1,4 +1,4 @@
-   
+
 
 var width = 400;	//画布的宽度
 var height = 400;	//画布的高度
@@ -10,7 +10,7 @@ var svg = d3.select("#container")				//选择文档中的body元素
     .attr("width", width)		//设定宽度
     .attr("height", height);	//设定高度
 
-var dataset = [2.3, 1.2, 2.4, 1.4, 0.8,3.2,4.3]
+var dataset = [2.3, 1.2, 2.4, 1.4, 0.8, 3.2, 4.3]
 
 var xScale = d3.scale.ordinal()
     .domain(d3.range(dataset.length))
@@ -31,7 +31,7 @@ var yAxis = d3.svg.axis()
 
 var rectPadding = 4
 
-svg.selectAll(".MyRect")
+var rects = svg.selectAll(".MyRect")
     .data(dataset)
     .enter()
     .append("rect")
@@ -45,58 +45,97 @@ svg.selectAll(".MyRect")
     })
     .attr("width", xScale.rangeBand() - rectPadding)
 
-    .attr("fill","steelblue")
+    .attr("fill", "steelblue")
+    // 监听事件要放在y的动画之前
+    .on("mouseover",function(d,i){
+        d3.select(this)
+            .attr("fill","yellow");
+    })
+    .on("mouseout",function(d,i){
+        d3.select(this)
+        .attr("fill","steelblue")
+    })
 
 
-    .attr("y",function(d){
+    .attr("y", function (d) {
         var min = yScale.domain()[0];
         return yScale(min)
     })
-    .attr("height",function(d){return 0})
+    .attr("height", function (d) { return 0 })
     .transition()
-    .delay(function(d,i){
-        return i*200
+    .delay(function (d, i) {
+        return i * 200
     })
     .duration(2000)
     .ease("bounce")
-    .attr("y",function(d){
+    .attr("y", function (d) {
         return yScale(d)
     })
     .attr("height", function (d) {
         return height - padding.top - padding.bottom - yScale(d);
     })
 
-    var texts = svg.selectAll(".MyText")
+    
+// var rects = svg.selectAll(".MyRect")
+//         .data(dataset)
+//         .enter()
+//         .append("rect")
+//         .attr("class","MyRect")   //把类里的 fill 属性清空
+//         .attr("transform","translate(" + padding.left + "," + padding.top + ")")
+//         .attr("x", function(d,i){
+//             return xScale(i) + rectPadding/2;
+//         } )
+//         .attr("y",function(d){
+//             return yScale(d);
+//         })
+//         .attr("width", xScale.rangeBand() - rectPadding )
+//         .attr("height", function(d){
+//             return height - padding.top - padding.bottom - yScale(d);
+//         })
+//         .attr("fill","steelblue")       //填充颜色不要写在CSS里
+        
+//         .on("mouseover",function(d,i){
+//             d3.select(this)
+//                 .attr("fill","#f10");
+//         })
+//         .on("mouseout",function(d,i){
+//             d3.select(this)
+//                 .transition()
+//                 .duration(500)
+//                 .attr("fill","steelblue");
+//         });
+
+var texts = svg.selectAll(".MyText")
     .data(dataset)
     .enter()
     .append("text")
-    .attr("class","MyText")
-    .attr("transform","translate(" + padding.left + "," + padding.top + ")")
-    .attr("x", function(d,i){
-        return xScale(i) + rectPadding/2;
-    } )
-    .attr("dx",function(){
-        return (xScale.rangeBand() - rectPadding)/2;
+    .attr("class", "MyText")
+    .attr("transform", "translate(" + padding.left + "," + padding.top + ")")
+    .attr("x", function (d, i) {
+        return xScale(i) + rectPadding / 2;
     })
-    .attr("dy",function(d){
+    .attr("dx", function () {
+        return (xScale.rangeBand() - rectPadding) / 2;
+    })
+    .attr("dy", function (d) {
         return 20;
     })
-    .text(function(d){
+    .text(function (d) {
         return d;
     })
 
 
-    .attr("y",function(d){
+    .attr("y", function (d) {
         var min = yScale.domain()[0];
         return yScale(min)
     })
     .transition()
-    .delay(function(d,i){
-        return i*200
+    .delay(function (d, i) {
+        return i * 200
     })
     .duration(2000)
     .ease("bounce")
-    .attr("y",function(d){
+    .attr("y", function (d) {
         return yScale(d)
     })
 
